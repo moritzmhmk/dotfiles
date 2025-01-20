3116847bf1e2555b95ccc0f696c92cde2bd2f60e
moritzmhmk <moritzmhmk@gmail.com>
2025-01-20 22:55:34 +0100

    Improve workspaces in sketchybar.

--- private_dot_config/sketchybar/scripts/executable_update_space.sh
+++ private_dot_config/sketchybar/scripts/executable_update_space.sh
@@ -2,35 +2,8 @@
 
 source $CONFIG_DIR/icon_map.sh
 
-space_id=$1
-space_color=$2
-
-if [[ "$SENDER" == "forced" ]]; then
-  space_item=(
-    icon=$space_id
-    icon.color=$space_color
-    icon.highlight_color=0xff1e2030
-    icon.padding_left=3
-    icon.padding_right=3
-    label.color=$space_color
-    label.highlight_color=0xff1e2030
-    label.font="sketchybar-app-font-bg:Regular:16.0"
-    label.padding_left=0
-    label.padding_right=1
-    background.color=$space_color
-    background.height=20
-    background.corner_radius=5
-    background.drawing=off
-    padding_left=3
-    padding_right=1
-  )
-
-  sketchybar --set $NAME "${space_item[@]}"
-fi
-
-
 if [[ "$SENDER" == "forced" || "$SENDER" == "space_windows_change" ]]; then
-  app_ids=$(aerospace list-windows --workspace $space_id --format %{app-bundle-id} | awk '!a[$0]++' -)
+  app_ids=$(aerospace list-windows --workspace $1 --format %{app-bundle-id} | awk '!a[$0]++' -)
   app_icons=$(for app_id in $app_ids; do
     echo "$(__icon_map "$app_id")"
   done)
@@ -52,4 +25,14 @@ if [[ -n "$ACTIVE_WORKSPACE" ]]; then
       background.drawing=off
   fi
 fi
+
+# TODO create event for "move-workspace-to-monitor"
+for space_info_str in $(aerospace list-workspaces --all --format "%{monitor-appkit-nsscreen-screens-id}:%{workspace}"); do
+  space_info_arr=(${space_info_str//:/ })
+  space_monitor=${space_info_arr[0]}
+  space_id=${space_info_arr[1]}
+  if [[ "$1" == "$space_id" ]]; then
+    sketchybar --set $NAME display=$space_monitor
+  fi
+done
 
